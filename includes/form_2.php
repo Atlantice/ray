c69a5a449337bce4ba8f233ab72b50e821d3180b
Atlantice <pereiram13@students.ecu.edu>
2016-12-19 15:34:23 -0500

    Test form

--- includes/form_2.php
+++ includes/form_2.php
@@ -1,23 +1,22 @@
 <?php	
-	if(empty($_POST['pname']) && strlen($_POST['pname']) == 0 || empty($_POST['oname']) && strlen($_POST['oname']) == 0 || empty($_POST['pemail']) && strlen($_POST['pemail']) == 0)
+	if(empty($_POST['cname']) && strlen($_POST['cname']) == 0 || empty($_POST['cemail']) && strlen($_POST['cemail']) == 0)
 	{
 		return false;
 	}
 	
-	$pname = $_POST['pname'];
-	$oname = $_POST['oname'];
-	$pemail = $_POST['pemail'];
-	$pmessage = $_POST['pmessage'];
+	$cname = $_POST['cname'];
+	$cemail = $_POST['cemail'];
+	$cmessage = $_POST['cmessage'];
 	
 	$to = 'pereiram13@students.ecu.edu'; // Email submissions are sent to this email
 
 	// Create email	
 	$email_subject = "Message from Ray.";
 	$email_body = "You have received a new message. \n\n".
-				  "Pname: $pname \nOname: $oname \nPemail: $pemail \nPmessage: $pmessage \n";
+				  "Cname: $cname \nCemail: $cemail \nCmessage: $cmessage \n";
 	$headers = "MIME-Version: 1.0\r\nContent-type: text/plain; charset=UTF-8\r\n";	
 	$headers .= "From: pereiram13@students.ecu.edu\n";
-	$headers .= "Reply-To: $pemail";	
+	$headers .= "Reply-To: $cemail";	
 	
 	mail($to,$email_subject,$email_body,$headers); // Post message
 	return true;			
